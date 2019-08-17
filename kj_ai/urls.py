2c35f5985644f58a97668849d83746002eade354
haten-hanata <hanatasfp@gmail.com>
2019-08-17 22:16:32 +0900

    8割完成

--- kj_ai/urls.py
+++ kj_ai/urls.py
@@ -7,7 +7,7 @@ app_name = 'kj_ai'
 
 urlpatterns = [
     path('input_url/', InputUrlView.as_view(), name='input_url'),
-    path('result/', ResultView.as_view(), name='result'),
+    path('result/<str:song_id>/', ResultView.as_view(), name='result'),
     # path('success/', index, name='success'),
     path('about/', AboutPageView.as_view(), name='about'),
 ]
--- kj_ai/views.py
+++ kj_ai/views.py
@@ -1,29 +1,27 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.views.generic import TemplateView, FormView, ListView, CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from .forms import *
 import os
 import pandas as pd
 import numpy as np
 from key_judge_app.settings import BASE_DIR
 import pickle
+from .my_function.scraping import func as scraping
+
 # UPLOAD_DIR = os.path.join(BASE_DIR, 'static/csv_files')
 
 
 class InputUrlView(FormView):
     template_name = 'kj_ai/input_url.html'
     form_class = InputUrlForm
-    success_url = reverse_lazy('kj_ai:about')
+    # success_url = reverse_lazy('kj_ai:result')
 
-    # def form_valid(self, form):
-    #     csv_file = form.cleaned_data['csv_file']
-    #     os.makedirs(UPLOAD_DIR, exist_ok=True)
-    #     save_path = os.path.join(UPLOAD_DIR, csv_file.name)
-    #     with open(save_path, 'wb') as destination:
-    #         for chunk in csv_file.chunks():
-    #             destination.write(chunk)
-    #     return super(SelectCsvView, self).form_valid(form)
+    def form_valid(self, form):
+        url = form.cleaned_data['url_str']
+        number = url.split('=')[-1]
+        return redirect('kj_ai:result',song_id=number)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -32,16 +30,17 @@ def index(request):
 class ResultView(TemplateView):
     template_name = 'kj_ai/result.html'
 
-
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        url = 'https://www.ufret.jp/song.php?data=' + kwargs['song_id']
+        df, song_name = scraping(url)
         model_pkl_dir = os.path.join(BASE_DIR, 'static/model/model.pkl')
+        le_pkl_dir = os.path.join(BASE_DIR, 'static/model/le.pkl')
         loaded_model = pickle.load(open(model_pkl_dir, 'rb'))
-
-
-
-        context['result'] = lst
-        context['ans'] = np.argmax(lst)
+        le = pickle.load(open(le_pkl_dir, 'rb'))
+        pred_y = loaded_model.predict(df)
+        context['answer'] = pred_y
+        context['song_name'] = song_name
         return context
 
 
